fb069a1340104f73b2bca4b41e1e6792c2d36d6e
Steven Turner <sturner1123@gmail.com>
2015-03-02 17:35:24 -0500

    fixed sub tests

--- sub_tests.java
+++ sub_tests.java
@@ -1,30 +1,46 @@
-import org.openqa.selenium.*;
+package com.example.tests;
+
+import java.util.concurrent.TimeUnit;
 import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class sub_tests{
 	public WebDriver driver = new FirefoxDriver();
 
 	@Before
 	public void setup_user(){
-		driver.get(cs1699test.reddit.com);
-		//driver.findElement(By.linkText("logout")).click();
-		driver.findElement(By.name("user")).click();
-		driver.findElement(By.name("user")).clear();
-		driver.findElement(By.name("user")).sendKeys("cs1699testuser");
-		driver.findElement(By.name("passwd")).click();
-		driver.findElement(By.name("passwd")).clear();
-		driver.findElement(By.name("passwd")).sendKeys("cs1699");
-		driver.findElement(By.cssSelector("button.btn")).click();
+		driver = new FirefoxDriver();
+	    baseUrl = "http://www.reddit.com/";
+	    driver.get(baseUrl);
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    try
+	    {
+	    	driver.findElement(By.linkText("logout")).click();
+			driver.findElement(By.name("user")).click();
+			driver.findElement(By.name("user")).clear();
+			driver.findElement(By.name("user")).sendKeys("cs1699testuser");
+			driver.findElement(By.name("passwd")).click();
+			driver.findElement(By.name("passwd")).clear();
+			driver.findElement(By.name("passwd")).sendKeys("cs1699");
+			driver.findElement(By.cssSelector("button.btn")).click();
+	    }
+	    catch(NoSuchElementException e)
+	   	{
+		   
+	   	}
 	}
 
 	@After
 	public void teardown_user(){
-		driver.findElement(By.linkText("logout")).click(); //log out once we're done testing the user
+		driver.quit();
 	}
 
 	@Test
 	public void test_subscribe(){
 		//press subsribe button
+		driver.get(baseUrl + "/r/cs1699test/");
 		driver.findElement(By.linkText("subscribe")).click();
 	    driver.get("www.reddit.com/subreddits/");
 	    // Warning: assertTextPresent may require manual changes
@@ -35,6 +51,7 @@ public class sub_tests{
 	@Test
 	public void test_unsubscribe(){
 		//press unsubscribe button
+		driver.get(baseUrl + "/r/cs1699test/");
 	    driver.findElement(By.linkText("unsubscribe")).click();
 	    driver.get("www.reddit.com/subreddits");
 	    // Warning: assertTextNotPresent may require manual changes
